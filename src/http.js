9178b0a83f778113d0d1d93935ffae538aaac917
ztimson <zaktimson@gmail.com>
2024-10-28 20:28:50 +0000

    Added bms monitoring script

--- src/http.js
+++ src/http.js
@@ -9,17 +9,20 @@ export default class Http extends Controller {
   constructor(apollo, port = 8000) {
     super(apollo);
     this.express = express();
+
+    this.express.get('/api/*', async (req, res) => {
+      const cmd = req.params['0'];
+      console.log(cmd);
+      res.json(await this.run(cmd));
+    });
+
     this.express.get('*', (req, res) => {
        let p = req.params['0'];
        if(!p || p == '/') p = 'index.html';
        const absolute = path.join(import.meta.url, '/../../ui/', p).replace('file:', '');
        res.sendFile(absolute);
     });
-    this.express.get('/api/*', async (req, res) => {
-      const cmd = req.params['0'];
-      console.log(cmd);
-      res.json(await this.run(cmd));
-    });
+
     this.express.listen(port);
   }
 }
--- src/sensor-suite.js
+++ src/sensor-suite.js
@@ -21,23 +21,27 @@ export default class SensorSuite {
     set data(d) { this._data = d; }
     get data() { return {timestamp: new Date().getTime(), ...this._data}; }
 
-    get status() { return { sensors: 'ok' }; }
+    _env_sensor = false;
+    get status() { return { env_sensor: this._env_sensor ? 'ok' : 'failed' }; }
 
     constructor() {
         this.bme280 = new BME280({i2cBusNo: 1, i2cAddress: 0x76});
     }
 
-    async start() {
-        await this.bme280.init();
-
-        // Poll environmental data
-        this.stopBme280 = poll(async () => {
-            const d = await this.bme280.readSensorData();
-            this._data.humidity = d.humidity / 100;
-            this._data.temperature = d.temperature_C;
-            this._data.pressure = d.pressure_hPa;
-            this._data.altitude = BME280.calculateAltitudeMeters(this.data.pressure);
-        }, 1000);
+    start() {
+        return this.bme280.init().then(() => {
+          // Poll environmental data
+          this._env_sensor = true;
+          this.stopBme280 = poll(async () => {
+              const d = await this.bme280.readSensorData();
+              this._data.humidity = d.humidity / 100;
+              this._data.temperature = d.temperature_C;
+              this._data.pressure = d.pressure_hPa;
+              this._data.altitude = BME280.calculateAltitudeMeters(this.data.pressure);
+          }, 1000);
+        }).catch(err => {
+          this._env_sensor = false;
+        });
     }
 
     stop() {
